0dcc5a0febb27c13d830d1f7f0ec571c6e237b54
Arana-Jayavihan <aranajayavihan@gmail.com>
2024-07-08 14:20:33 +0530

    misc

--- start_tmp.sh
+++ start_tmp.sh
@@ -4,7 +4,7 @@ test=$(netstat -tulpn | grep 9092 | cut -d "/" -f 1 | cut -d "N" -f 2 )
 kill -9 $test;
 cd /path/to/folder
 source .venv/bin/activate;
-python main.py &
+python tunnel.py &
 sleep 3
 sshpass -p SSH_PASSWORD ssh -C -o "ProxyCommand=nc -X CONNECT -x 127.0.0.1:9092 %h %p" USERNAME@IP -p STUN_PORT -v -CN -D 1080 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null &
 # Optional HTTP proxy
