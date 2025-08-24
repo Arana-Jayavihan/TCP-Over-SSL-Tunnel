c3b79ccf81a67e6ad1c5c7784c0d81b4e9651dba
Arana-Jayavihan <aranajayavihan@gmail.com>
2025-08-24 17:19:23 +0530

    misc

--- start_tmp.sh
+++ start_tmp.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-test=$(netstat -tulpn | grep 9092 | cut -d "/" -f 1 | cut -d "N" -f 2 )
-kill -9 $test;
+SSH_PASSWORD=
+USERNAME=
+STUN_PORT=
+HOST=
+HTTP=true
+
+pid=$(netstat -tulpn | grep 9092 | cut -d "/" -f 1 | cut -d "N" -f 2 )
+kill -9 $pid;
 cd /path/to/folder
 source .venv/bin/activate;
 python tunnel.py &
 sleep 3
-sshpass -p SSH_PASSWORD ssh -C -o "ProxyCommand=nc -X CONNECT -x 127.0.0.1:9092 %h %p" USERNAME@IP -p STUN_PORT -v -CN -D 1080 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null &
+sshpass -p $SSH_PASSWORD ssh -C -o "ProxyCommand=nc -X CONNECT -x 127.0.0.1:9092 %h %p" $USERNAME@$HOST -p $STUN_PORT -CN -D 1080 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null &
 # Optional HTTP proxy
-sleep 1
-pproxy -l http://0.0.0.0:1090 -r socks5://127.0.0.1:1080 > /dev/null
+if [ "$HTTP" == "true" ]; then
+  sleep 1
+  echo "[+] HTTP Proxy Running on 127.0.0.1:1090"
+  pproxy -l http://0.0.0.0:1090 -r socks5://127.0.0.1:1080 > /dev/null
+f1
